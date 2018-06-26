8ab9ebce31fc0d9d133ceb1d9daf22f30a5ee21a
hzfran <hillarye37@yahoo.com>
2018-06-25 19:51:50 -0700

    added all 12 notes

--- note_finder.py
+++ note_finder.py
@@ -8,6 +8,9 @@ from NoteFinder.plotting.bullseye_plot import Plot
 wav_file_test = './NoteFinder/media/Gmaj.wav'
 notes = ""
 
+
+
+
 def main():
 
     """
@@ -22,65 +25,66 @@ def main():
     # create Plot object:
     plot = Plot()
 
-    # launch blank plot
+    # create blank plot
     plot.start_plot()
 
     # create FFT object
     # fa_array= FFT(input_file=args.input_file)
     fa_array = FFT(input_file=wav_file_test)
 
-
-    # run FFT.run to extract loudest freq within the file
-    loudest_freq1, amplitude1 = fa_array.max_freq()
-
-
     # create Note object
     fnote_converter = Note()
 
-    # run Note.run to return note and freq range of note and freq ranges of all harmonics of the note:
-    note1, freq_range1, freq_range_all1, freq_harm1 = fnote_converter.run(loudest_freq1)
+    def process_audio_stats(amp_array=None):
 
-    note_amps1 = fa_array.amp_all_harm(freq_harm1)
+        if type(amp_array).__module__ == "builtins":
 
-    # plot.plot_amp(note1, note_amps1)
+            # run FFT.run to extract loudest freq within the file
+            loudest_freq, amplitude = fa_array.max_freq()
+        else:
+            loudest_freq, amplitude = fa_array.max_freq(amp_array=amp_array)
 
-    #create a new array with the amplitude set to masked for all frequencies within range of the note:
+        # run Note.run to return note and freq range of note and freq ranges of all harmonics of the note:
+        note, freq_range, freq_range_all, freq_harm = fnote_converter.run(loudest_freq)
 
-    amp_array2 = fa_array.filter_out_freq_range(freq_range_all1)
+        # returns array of amplitudes (12 total) for all harmonics of the note (inputs an array of freqs):
+        note_amps = fa_array.amp_all_harm(freq_harm)
 
-    loudest_freq2, amplitude2 = fa_array.max_freq(amp_array=amp_array2)
-    note2, freq_range2, freq_range_all2, freq_harm2 = fnote_converter.run(loudest_freq2)
+        # returns new array with previous highest amp set to masked:
+        if type(amp_array).__module__ == "builtins":
+            filtered_array = fa_array.filter_out_freq_range(freq_range_all)
+        else:
+            filtered_array = fa_array.filter_out_freq_range(freq_range_all, amp_array=amp_array)
 
-    note_amps2 = fa_array.amp_all_harm(freq_harm2)
+        return note, note_amps, filtered_array
 
-    amp_array3 = fa_array.filter_out_freq_range(freq_range_all2, amp_array=amp_array2)
-
-    loudest_freq3, amplitude3 = fa_array.max_freq(amp_array=amp_array3)
-    note3, freq_range3, freq_range_all3, freq_harm3 = fnote_converter.run(loudest_freq3)
-
-    note_amps3 = fa_array.amp_all_harm(freq_harm3)
-
-    amp_array4 = fa_array.filter_out_freq_range(freq_range_all3, amp_array=amp_array3)
-
-    loudest_freq4, amplitude4 = fa_array.max_freq(amp_array=amp_array4)
-    note4, freq_range4, freq_range_all4, freq_harm4 = fnote_converter.run(loudest_freq4)
-
-    note_amps4 = fa_array.amp_all_harm(freq_harm4)
+    ## run process_audio_stats x12 to get all 12 notes:
+    note1, note_amps1, amp_array2 = process_audio_stats()
+    note2, note_amps2, amp_array3 = process_audio_stats(amp_array=amp_array2)
+    note3, note_amps3, amp_array4 = process_audio_stats(amp_array=amp_array3)
+    note4, note_amps4, amp_array5 = process_audio_stats(amp_array=amp_array4)
+    note5, note_amps5, amp_array6 = process_audio_stats(amp_array=amp_array5)
+    note6, note_amps6, amp_array7 = process_audio_stats(amp_array=amp_array6)
+    note7, note_amps7, amp_array8 = process_audio_stats(amp_array=amp_array7)
+    note8, note_amps8, amp_array9 = process_audio_stats(amp_array=amp_array8)
+    note9, note_amps9, amp_array10 = process_audio_stats(amp_array=amp_array9)
+    note10, note_amps10, amp_array11 = process_audio_stats(amp_array=amp_array10)
+    note11, note_amps11, amp_array12 = process_audio_stats(amp_array=amp_array11)
+    note12, note_amps12, amp_array13 = process_audio_stats(amp_array=amp_array12)
 
     notes = note1[:-1] + " " + note2[:-1] + " " + note3[:-1]
 
 
-    #create 2d arrays all notes (in order of detected) and for amplitudes of all notes:
+    ## create 2d arrays all notes (in order of detected) and for amplitudes of all notes:
 
-    all_notes = np.array([[note1], [note2], [note3], [note4]])
+    all_notes = np.array([[note1], [note2], [note3], [note4], [note5], [note6], [note7], [note8], [note9], [note10], [note11], [note12]])
 
-    all_amps = np.array([note_amps1, note_amps2, note_amps3, note_amps4])
+    all_amps = np.array([note_amps1, note_amps2, note_amps3, note_amps4, note_amps5, note_amps6, note_amps7, note_amps8, note_amps9, note_amps10, note_amps11, note_amps12])
 
     plot.plot_amp(all_notes, all_amps)
 
 
-
-
+    ## input 3 highest notes found to get chord:
     chord = fnote_converter.chord(notes)
     print(chord)
 
